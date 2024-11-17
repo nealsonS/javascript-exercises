68b3dcb8e16b10214389773c3c05350fe25b3847
Nealson Setiawan <nealsonsetiawan03@gmail.com>
2024-11-17 09:09:59 -0800

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(
+    (a,b) => a+b, 0
+  )
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(
+    (a,b) => a*b, 1
+  )
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  out = 1
+  for (let i=1; i <= a; i++){
+    out = out * i
+    
+  }
+  return out
 };
 
 // Do not edit below this line
